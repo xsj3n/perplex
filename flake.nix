bc21b48dcdbb4d26f52e0cf0fd6b2adba61fccf1
xsj3n <xsj3n@tutanota.com>
2026-02-08 23:38:50 -0700

    prepared a docker container build for what will be the server

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
-  description = "Perplexity CLI ripper script";
+  description = "Perplexity CLI ripper";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
   };
 
-  outputs = { self , nixpkgs, ... }: 
+  outputs = { nixpkgs, ... }:
   let
     system = "x86_64-linux";
-    libraries = with pkgs;[]; 
     pkgs = import nixpkgs { inherit system; };
-    pyPkgs = pkgs.python313Packages; 
+    pyPkgs = pkgs.python314Packages;
+    pyEnv = pkgs.python314.withPackages (p: with p; [
+      selenium
+      undetected-chromedriver
+    ]);
   in 
   {
     devShells."${system}".default = pkgs.mkShell {
@@ -26,5 +29,45 @@
         echo "${pkgs.undetected-chromedriver}" > "driver-path.txt"
       '';
     };
+
+    packages."${system}".default = pkgs.dockerTools.buildLayeredImage {
+      name = "perplex-ct";
+      tag = "latest";
+      contents = with pkgs; [
+        chromium
+        xorg.libX11
+        xorg.libXext
+        xorg.libXrender
+        fontconfig
+        freetype
+        glib
+        nss
+        xvfb-run
+        cacert
+        dbus
+        pyEnv
+        (pkgs.writeTextFile {
+          name = "script.py";
+          destination ="/app/script.py";
+          text = builtins.readFile ./main.py;
+        })
+      ];
+
+      extraCommands = ''
+        mkdir -p tmp/
+        chmod 1777 tmp/ 
+      '';
+      
+      config = {
+        Env = [ "DISPLAY=:99" ];
+        Cmd = [
+          "xvfb-run"
+          "--server-args=-screen 0 1920x1080x24"
+          "${pyEnv}/bin/python"
+          "/app/script.py"
+        ];
+      };
+    };
+    
   };
 }
