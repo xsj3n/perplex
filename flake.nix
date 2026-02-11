0000d98b391d070b0f2fad3dbb72cb70c5fee1be
xsj3n <xsj3n@tutanota.com>
2026-02-11 09:33:54 -0700

    flake mods for building package

--- flake.nix
+++ flake.nix
@@ -6,83 +6,65 @@
     nixpkgs-cabal31420.url = "github:nixos/nixpkgs/f4b140d5b253f5e2a1ff4e5506edbf8267724bde";
   };
 
-  outputs = { nixpkgs, nixpkgs-cabal31420, ... }:
+  outputs = { self, nixpkgs, nixpkgs-cabal31420, ... }:
   let
     system = "x86_64-linux";
-    haskell_src = ./.; 
+    src = ./.; 
     name = "perplex-client";
     pkgs = import nixpkgs { inherit system; };
     c31420 = import nixpkgs-cabal31420 { inherit system; };
     pyPkgs = pkgs.python314Packages;
     hkPkgs = pkgs.haskellPackages;
-    pyEnv = pkgs.python314.withPackages (p: with p; [
-      selenium
-      undetected-chromedriver
-    ]);
+    pyInputs = [
+      pkgs.python314
+      pyPkgs.selenium
+      pyPkgs.python-lsp-server
+      pyPkgs.undetected-chromedriver
+      pkgs.chromium
+      pkgs.xvfb-run
+    ];
+    hkInputs = [
+      hkPkgs.haskell-language-server
+      c31420.cabal-install
+      pkgs.zlib
+      pkgs.haskell.compiler.native-bignum.ghcHEAD
+    ];
+
+    client = hkPkgs.callCabal2nix "${name}" src { };
+
+    server = pkgs.stdenv.mkDerivation {
+      name = "perplex-selenium-server";
+      src = src;
+      buildInputs = pyInputs;
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/bin $out/share/server/
+        cp ${src}/main.py $out/share/server/
+        echo "${pkgs.undetected-chromedriver}" > $out/share/server/driver-path.txt
+      '';
+    };
   in 
   {
     devShells."${system}".default = pkgs.mkShell {
-      packages = [
-        pkgs.python314
-        pyPkgs.selenium
-        pyPkgs.python-lsp-server
-        pyPkgs.undetected-chromedriver
-        pkgs.chromium
-        hkPkgs.haskell-language-server
-        c31420.cabal-install
-        pkgs.zlib
-        pkgs.haskell.compiler.native-bignum.ghcHEAD
-        hkPkgs.hoogle
-        pkgs.xvfb-run
-      ];
-
+      packages = hkInputs ++ pyInputs;
       shellHook = ''
         echo "${pkgs.undetected-chromedriver}" > "driver-path.txt"
       '';
     };
 
-    packages."${system}" = {
-
-      default = hkPkgs.callCabal2nix "${name}" haskell_src { };
-
-      ct = pkgs.dockerTools.buildLayeredImage {
-        name = "perplex-ct";
-        tag = "latest";
-        contents = with pkgs; [
-          chromium
-          xorg.libX11
-          xorg.libXext
-          xorg.libXrender
-          fontconfig
-          freetype
-          glib
-          nss
-          xvfb-run
-          cacert
-          dbus
-          pyEnv
-          (pkgs.writeTextFile {
-            name = "script.py";
-            destination ="/app/script.py";
-            text = builtins.readFile ./main.py;
-          })
-        ];
-
-        extraCommands = ''
-          mkdir -p tmp/
-          chmod 1777 tmp/ 
+    packages."${system}".default = pkgs.symlinkJoin {
+        name = "perplex";
+        paths = [ server ];
+        buildInputs = [ pkgs.bash pkgs.xvfb-run client];
+        postBuild = ''
+          cat > $out/bin/perplex <<'EOF'
+          #!/usr/bin/env sh
+          ${pkgs.xvfb-run} python ${server}/share/server/main.py &
+          exec ${client}/bin/${name}
+          EOF
+          chmod +x $out/bin/perplex
         '';
-      
-        config = {
-          Env = [ "DISPLAY=:99" ];
-          Cmd = [
-            "xvfb-run"
-            "--server-args=-screen 0 1920x1080x24"
-            "${pyEnv}/bin/python"
-            "/app/script.py"
-          ];
-        };
-      };
     };
+      
   };
 }
